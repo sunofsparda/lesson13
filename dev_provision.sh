97660a416f035db23f1a21080604ca2ef5133678
Anton Cherlyonok <sunofsparda@gmail.com>
2017-04-17 15:06:32 +0300

    test1

--- dev_provision.sh
+++ dev_provision.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-grep -q -F '192.168.100.102 puppet-node1' /etc/hosts || echo '192.168.100.102 puppet-node1.minsk.epam.com' >> /etc/hosts
+grep -q -F '192.168.100.102 puppet-node1' /etc/hosts || echo '192.168.100.102 puppet-node1' >> /etc/hosts
 
 yum install -y epel-release > /dev/null 2>&1
 yum localinstall -y http://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm > /dev/null 2>&1
 yum install -y puppetserver > /dev/null 2>&1
 yum install -y nginx > /dev/null 2>&1
 
-cp /vagrant/puppet/site.pp /etc/puppetlabs/code/environments/production/manifests
-cp /vagrant/puppet/autosign.conf /etc/puppetlabs/puppet/
+/bin/cp /vagrant/puppet/site_pp.prod /etc/puppetlabs/code/environments/production/manifests/site.pp
+/bin/cp /vagrant/puppet/autosign.conf /etc/puppetlabs/puppet/autosign.conf
+/bin/cp /vagrant/puppet/puppet_conf.prod /etc/puppetlabs/puppet/puppet.conf
 
 systemctl enable puppetserver
 systemctl start puppetserver
+
+yum install -y http://yum.postgresql.org/9.4/redhat/rhel-7-x86_64/pgdg-redhat94-9.4-2.noarch.rpm
+yum install -y postgresql94-server postgresql94-contrib
+/usr/pgsql-9.4/bin/postgresql94-setup initdb
+/bin/cp /vagrant/puppet/pg_hba.conf /var/lib/pgsql/9.4/data/
+systemctl enable postgresql-9.4.service
+systemctl start postgresql-9.4.service
+sudo -u postgres psql -c "create user puppetdb password 'puppetdb'"
+sudo -u postgres psql -c "create database puppetdb owner puppetdb"
 
 PATH=/opt/puppetlabs/bin:$PATH;export PATH
 
@@ -19,13 +29,13 @@ PATH=/opt/puppetlabs/bin:$PATH;export PATH
 firewall-cmd --zone=public--add-port=8081/tcp --permanent 
 firewall-cmd --reload
 
-# PuppetDB 2.3 » Installing PuppetDB From Packages
-puppet resource package puppetdb ensure=latest
-puppet resource service puppetdb ensure=running enable=true
-
-# PuppetDB 2.3 » Connecting Puppet Masters to PuppetDB
+# PuppetDB » Connecting Puppet Masters to PuppetDB
 puppet resource package puppetdb-terminus ensure=latest
 
-# Step 2: Edit Config Files
-# Locate Puppet’s Config Directory
-# puppet config print confdir
+# PuppetDB » Installing PuppetDB From Packages
+puppet module install puppetlabs-puppetdb --version 5.1.2
+puppet module install puppetlabs-mysql --version 3.10.0 --environment production
+puppet module install puppetlabs-apache --version 1.11.0
+puppet module install spotify-puppetexplorer --version 1.1.1
+puppet module install puppet-nginx --version 0.6.0 --environment production
+puppet agent -t --verbose

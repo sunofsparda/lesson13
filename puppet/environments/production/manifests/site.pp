b8319bdbfcdf6d44589a89a444c3924eeb3cfe71
Anton Cherlyonok <sunofsparda@gmail.com>
2017-04-19 00:41:35 +0300

    Update site.pp

--- puppet/environments/production/manifests/site.pp
+++ puppet/environments/production/manifests/site.pp
@@ -7,9 +7,9 @@ node 'server.lab' {
   # Configure the Puppet master to use puppetdb
   class { 'puppetdb::master::config': }
   class { 'puppetexplorer':
+  class {'::puppetexplorer':
     vhost_options => {
-      rewrites => [ { rewrite_rule => [
-        '^/api/metrics/v1/mbeans/puppetlabs.puppetdb.query.population:type=default,name=(.*)$  https://%{HTTP_HOST}/api/metrics/v1/mbeans/puppetlabs.puppetdb.population:name=$1 [R=301,L]'
-      ] } ] }
+      rewrites  => [ { rewrite_rule => ['^/api/metrics/v1/mbeans/puppetlabs.puppetdb.query.population:type=default,name=(.*)$  https://%{HTTP_HOST}/api/metrics/v1/mbeans/puppetlabs.puppetdb.population:name=$1 [R=301,L]'] } ] }
+    }
   }
 }

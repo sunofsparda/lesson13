0e26268ce4d79a0616fca0c9d4bdcf25429c83a3
Anton Cherlyonok <sunofsparda@gmail.com>
2017-04-19 11:49:02 +0300

    Update node_provision.sh

--- node_provision.sh
+++ node_provision.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 grep -q -F '192.168.100.101 server.lab' /etc/hosts || echo '192.168.100.101 server.lab' >> /etc/hosts
 grep -q -F '192.168.100.102 node.lab' /etc/hosts || echo '192.168.100.102 node.lab' >> /etc/hosts
 
@@ -8,7 +6,7 @@ yum localinstall -y http://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch
 yum install -y puppet-agent > /dev/null 2>&1
 
 /bin/cp /vagrant/puppet/puppet_conf.node /etc/puppetlabs/puppet/puppet.conf
-source ~/.bashrc
+# source ~/.bashrc
 puppet agent -t --verbose
 
 echo 'provisioned'
